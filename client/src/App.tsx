eeae410485a568797866741bb98d4df6b2b31894
Dag B. Rovik <dag@iseesharp.net>
2026-04-21 20:40:43 +0200

    feat: wrap app with ThemeProvider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,17 @@
 import { HashRouter, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from './context/ThemeContext';
 import { Home } from './pages/Home';
 import { Room } from './pages/Room';
 
 export default function App() {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/room/:roomId" element={<Room />} />
-      </Routes>
-    </HashRouter>
+    <ThemeProvider>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/room/:roomId" element={<Room />} />
+        </Routes>
+      </HashRouter>
+    </ThemeProvider>
   );
 }
