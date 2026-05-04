fac55682a4952d6c498c0e932fee4bf89d34213d
Dag B. Rovik <dag@iseesharp.net>
2026-05-04 21:42:55 +0200

    feat: add 0.5 and infinity cards

--- client/src/types.ts
+++ client/src/types.ts
@@ -5,5 +5,5 @@ export type Participant = {
   observer?: boolean;
 };
 
-export const CARD_VALUES = ['1', '2', '3', '5', '8', '13', '21', '?', '☕'] as const;
+export const CARD_VALUES = ['0.5', '1', '2', '3', '5', '8', '13', '21', '∞', '?', '☕'] as const;
 export type CardValue = (typeof CARD_VALUES)[number];
